d5137c7973d44201aaed7c56fcad0f4a0b550419
413831 <alepleins3pp@gmail.com>
2024-10-16 00:31:52 -0300

    add EJS template engine basic logic

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
-const fs = require('fs');
-const audiosRoutes = require('./src/routes/audiosRoutes.js');
+const subscriptionRoutes = require('./src/routes/subscriptionRoutes.js');
 const usuariosRoutes = require('./src/routes/usuariosRoutes.js');
+const mainRoutes = require('./src/routes/mainRoutes.js');
 
 app.use(express.urlencoded());
 app.use(express.json());
 
+// config template engines
+app.set('view engine','ejs');
+app.set('views','./src/views')
+
 // routes
 app.get('/ping',(req, res) => res.send('pong'));
 
-app.get('/',(req, res) => res.send('Bienvenido \n <h2>Presentación Rutas</h2>'));
-
-app.get('/home',(req, res) => {
-    res.sendFile(__dirname+'/public/pages/index.html');
-});
-
-app.use('/media', audiosRoutes);
+app.use('/plans', subscriptionRoutes);
 app.use('/usuarios', usuariosRoutes);
+app.use('/', mainRoutes);
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -18,25 +18,25 @@ router.get('/usuarios/:id', (req,res) => {
 
     const usuariosArray = JSON.parse(getUsuario);
 
-    let audioRes = null;
+    let SubscriptionRes = null;
 
-    usuariosArray.forEach(audio => {
-        if(audio.id === id) {
-            audioRes = audio;
+    usuariosArray.forEach(Subscription => {
+        if(Subscription.id === id) {
+            SubscriptionRes = Subscription;
         }  
     });
 
-    if(audioRes) {
-        res.status(200).json(audioRes);
+    if(SubscriptionRes) {
+        res.status(200).json(SubscriptionRes);
     }
     else {
-        res.status(404).json({message:`El audio con id ${id} no se ha encontrado`});
+        res.status(404).json({message:`El Subscription con id ${id} no se ha encontrado`});
     }
 });
 
 router.post('/usuarios', (req,res) => {
 
-    const newAudio = req.body; 
+    const newSubscription = req.body; 
 
     let usuariosArray;
 
@@ -47,21 +47,21 @@ router.post('/usuarios', (req,res) => {
         return res.status(500).json({ message: 'Error leyendo el archivo JSON'});
     }
 
-    usuariosArray.push(newAudio);
+    usuariosArray.push(newSubscription);
 
     try {
         fs.writeFileSync('./data/usuarios.json', JSON.stringify(usuariosArray, null, 2));
-        res.status(200).json({ message: 'Audio agregado exitosamente' });
+        res.status(200).json({ message: 'Subscription agregado exitosamente' });
     } catch (error) {
         res.status(500).json({ message: 'Error escribiendo en el archivo JSON' });
     }
 });
 
 router.put('/usuarios/:id', (req, res) => {
-    const audioId = parseInt(req.params.id, 10); // Obtener el id del parámetro de la URL
-    const updatedAudio = req.body; // Obtener los datos actualizados del cuerpo de la solicitud
+    const SubscriptionId = parseInt(req.params.id, 10); // Obtener el id del parámetro de la URL
+    const updatedSubscription = req.body; // Obtener los datos actualizados del cuerpo de la solicitud
 
-    if (isNaN(audioId)) {
+    if (isNaN(SubscriptionId)) {
         return res.status(400).json({ message: 'ID inválido' });
     }
 
@@ -74,27 +74,27 @@ router.put('/usuarios/:id', (req, res) => {
         return res.status(500).json({ message: 'Error leyendo el archivo JSON' });
     }
 
-    const index = usuariosArray.findIndex(audio => audio.id === audioId);
+    const index = usuariosArray.findIndex(Subscription => Subscription.id === SubscriptionId);
 
     if (index === -1) {
-        return res.status(404).json({ message: `El audio con ID ${audioId} no se ha encontrado` });
+        return res.status(404).json({ message: `El Subscription con ID ${SubscriptionId} no se ha encontrado` });
     }
 
-    // Actualizar el audio encontrado
-    usuariosArray[index] = { ...usuariosArray[index], ...updatedAudio };
+    // Actualizar el Subscription encontrado
+    usuariosArray[index] = { ...usuariosArray[index], ...updatedSubscription };
 
     try {
         fs.writeFileSync('./data/usuarios.json', JSON.stringify(usuariosArray, null, 2), 'utf-8');
-        res.status(200).json({ message: 'Audio actualizado exitosamente' });
+        res.status(200).json({ message: 'Subscription actualizado exitosamente' });
     } catch (error) {
         res.status(500).json({ message: 'Error escribiendo en el archivo JSON' });
     }
 });
 
 router.delete('/usuarios/:id', (req, res) => {
-    const audioId = parseInt(req.params.id, 10); // Obtener el id del parámetro de la URL
+    const SubscriptionId = parseInt(req.params.id, 10); // Obtener el id del parámetro de la URL
 
-    if (isNaN(audioId)) {
+    if (isNaN(SubscriptionId)) {
         return res.status(400).json({ message: 'ID inválido' });
     }
 
@@ -107,15 +107,15 @@ router.delete('/usuarios/:id', (req, res) => {
         return res.status(500).json({ message: 'Error leyendo el archivo JSON' });
     }
 
-    const newUsuarioArray = usuariosArray.filter(audio => audio.id !== audioId);
+    const newUsuarioArray = usuariosArray.filter(Subscription => Subscription.id !== SubscriptionId);
 
     if (newUsuarioArray.length === usuariosArray.length) {
-        return res.status(404).json({ message: `El audio con ID ${audioId} no se ha encontrado` });
+        return res.status(404).json({ message: `El Subscription con ID ${SubscriptionId} no se ha encontrado` });
     }
 
     try {
         fs.writeFileSync('./data/usuarios.json', JSON.stringify(newUsuarioArray, null, 2), 'utf-8');
-        res.status(200).json({ message: 'Audio eliminado exitosamente' });
+        res.status(200).json({ message: 'Subscription eliminado exitosamente' });
     } catch (error) {
         res.status(500).json({ message: 'Error escribiendo en el archivo JSON' });
     }
